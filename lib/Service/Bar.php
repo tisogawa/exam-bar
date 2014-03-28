5b0814bd63f216f61c3bf8634693952e8d5e0e76
Tadasu Isogawa <ucb.rcdtokyo@gmail.com>
2014-03-28 16:45:39 +0900

    updated

--- lib/Service/Bar.php
+++ lib/Service/Bar.php
@@ -2,8 +2,15 @@
 
 namespace Exam\Service;
 
+/**
+ * Class Bar
+ * @package Exam\Service
+ */
 class Bar
 {
+    /**
+     * @return string
+     */
     public function doSomething()
     {
         return __METHOD__;
